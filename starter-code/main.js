d668dee9175eb1ff99e697e106b1d228306d266f
Renjith Vijayan <mackid@Renjiths-MacBook-Pro.local>
2016-10-11 17:02:34 -0400

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,10 +3,3 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-if ( cardOne == cardTwo ) {
-	alert( "You found a match" );
-}
-else if( cardThree == cardFour )
-	alert( "Yoiu have found a match" );
-else
-	alert( "Sorry, try again" );
